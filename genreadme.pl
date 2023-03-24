60b6e07a48ace5945a212a4063ffd120370f3d0d
Sumukh Prasad <sumukhprasad@icloud.com>
2023-03-24 06:43:09 +0530

    Fourth commit, finally fixed readme.

--- genreadme.pl
+++ genreadme.pl
@@ -3,7 +3,7 @@ use strict; use warnings;
 open my $styfh, '<', 'colorpalette.sty' or die "Whoops!";
 my $styfile = do { local $/; <$styfh> };
 my @matches;
-push @matches, "- ![#$2](https://placehold.co/15x15/#$2/#$2.png) `#$2` $1" while ( $styfile =~ /definecolor\{(.+)\}\{(?:.+)\}\{(.+)\}/g );
+push @matches, "- ![#$2](https://placehold.co/15x15/$2/$2.png) `#$2` $1" while ( $styfile =~ /definecolor\{(.+)\}\{(?:.+)\}\{(.+)\}/g );
 my $count = @matches;
 print "Found " . $count . " colours.\n";
 close($styfile);
@@ -20,7 +20,7 @@ open my $rmfh, '+<', 'README.md' or die "Whoops!";
 my $rmfile = do { local $/; <$rmfh> };
 seek($rmfh, 0, 0);
 print $rmfh $filledrmtemplate;
-truncate($rmfile, tell($rmfh));
+truncate($rmfh, length($filledrmtemplate));
 close($rmfile);
 
 print "Done.\n"
